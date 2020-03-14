d5bbf4c8863763c2a40e5002d3a782584a808cc3
AlphaNiner1415 <manchesternd@gmail.com>
2020-03-14 17:22:59 +0700

    CSS Modules styling applied to Person class

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,31 +1,19 @@
 import React from 'react';
-// import './Person.css';
-import styled from 'styled-components';
-
+import classes from './Person.css';
 //    Dumb/ Presentational components because they have no internal logic or states.
 
-const StyleDiv = styled.div`
-        width: 60%;
-        margin: 16px auto;
-        border: 1px solid#eee;
-        box-shadow: 4px 5px 2px #ccc;
-        padding: 16px;
-        text-align: center;
 
-        @media (min-width: 500px){
-                width: 450px;
-        }`
 const person = (props) => {
     return (
       // <div title= {props.name} className="Person" style={style}>
-      <StyleDiv>
+      <div className={classes.Person}>
         <p onClick={props.click}>
           I'm {props.name} and I am {props.age} years old!!!
         </p>
         <p>{props.children}</p>{" "}
         {/*Anything passed in between the opening and closing tags*/}
         <input type="text" onChange={props.changed} value={props.name} />
-      </StyleDiv>
+      </div>
     );
     
 }
