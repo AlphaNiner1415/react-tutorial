f27914e9fa7f892010a27d5669765be9235c8fae
AlphaNiner1415 <manchesternd@gmail.com>
2020-06-28 16:08:13 +0700

    Logical Error Fixed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
     }
     nameChangeHandler = (event, id ) => {
         const personIndex = this.state.persons.findIndex(p => {
-            return p.id === id; //Logical Error userId is undefined
+            return p.id === id; 
         });
         //const person = Object.assign({}, this.state.persons[personIndex]);
         const templatePerson = {
