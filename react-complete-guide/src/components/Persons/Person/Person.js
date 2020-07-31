a4b3a3e6f4099e9fbfc3384ec458b826cee7fb65
AlphaNiner1415 <manchesternd@gmail.com>
2020-07-31 13:07:09 +0700

    Person changed to class-based

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 //    Dumb/ Presentational components because they have no internal logic or states.
 
 //Finished, no need to split into smaller components
-const person = (props) => {
-  console.log('[Person.js] rendering....');
-  return (
-    
-    // <div title= {props.name} className="Person" style={style}>
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!!!
+class Person extends Component {
+  render(){
+    console.log('[Person.js] rendering....');
+    return (
+
+      // <div title= {props.name} className="Person" style={style}>
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!!!
       </p>
-      <p>{props.children}</p>{" "}
-      {/*Anything passed in between the opening and closing tags*/}
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
+        <p>{this.props.children}</p>{" "}
+        {/*Anything passed in between the opening and closing tags*/}
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
+  
     
 }
 
 
-export default (person);
+export default (Person);
