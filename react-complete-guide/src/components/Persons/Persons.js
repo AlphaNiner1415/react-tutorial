ffa6fc089ebe2516fe07f4fe425d1ba1fcebd256
AlphaNiner1415 <manchesternd@gmail.com>
2020-07-31 13:18:05 +0700

    Added Component update lifecycle methods

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 class Persons extends Component {
+    // static getDerivedStateFromProps(props,state){
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state; //Which is going to be an empty object
+    // }
+
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true; //Should component update
+    }    
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return {message: 'Snapshot!'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(snapshot);
+        console.log('[Persons.js] componentDidUpdate');
+    }
+
+    
     render() {
         console.log('[Persons.js] rendering...');
         return this.props.persons.map((eachPerson, index) => {
