0dae0631e7f96d3d50937c327bb29b93e66f8353
AlphaNiner1415 <manchesternd@gmail.com>
2020-07-31 13:09:30 +0700

    Persons changed to class-based and componentWillMount() removed

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
-    return props.persons.map((eachPerson, index) => {
-            return ( 
+class Persons extends Component {
+    render() {
+        console.log('[Persons.js] rendering...');
+        return this.props.persons.map((eachPerson, index) => {
+            return (
                 <Person
-                click={ () => props.clicked( index ) }
-                name={eachPerson.name}
-                age={eachPerson.age}
-                key={eachPerson.id}
-                changed={ ( event ) => props.changed(event, eachPerson.id) } /> //Keys help react know what to render easily without having to inspect deeply inside one element
+                    click={() => this.props.clicked(index)}
+                    name={eachPerson.name}
+                    age={eachPerson.age}
+                    key={eachPerson.id}
+                    changed={(event) => this.props.changed(event, eachPerson.id)} /> //Keys help react know what to render easily without having to inspect deeply inside one element
             );
-    }); 
+        }); 
+    }
+    
 }
-export default persons;
+export default Persons;
