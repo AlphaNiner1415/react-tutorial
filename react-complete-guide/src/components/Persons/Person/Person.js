b0583f014c245776e2ba3960a43f6ac6478b3bcf
AlphaNiner1415 <manchesternd@gmail.com>
2020-08-02 17:01:09 +0700

    Person.js changed to return an array instead

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,18 +6,19 @@ import classes from './Person.css';
 class Person extends Component {
   render(){
     console.log('[Person.js] rendering....');
-    return (
-
-      // <div title= {props.name} className="Person" style={style}>
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+    return [
+ // <div title= {props.name} className="Person" style={style}>
+        <p key="i1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!!!
-      </p>
-        <p>{this.props.children}</p>{" "}
-        {/*Anything passed in between the opening and closing tags*/}
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
-    );
+        </p>,
+        <p key="i2">{this.props.children}</p>,
+        <input 
+          key="i3" 
+          type="text" 
+          onChange={this.props.changed} 
+          value={this.props.name} 
+        />
+    ];
   }
   
     
