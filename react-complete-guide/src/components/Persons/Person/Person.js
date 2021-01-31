f6efec7aa862be7b9adb71bae86a135a3a11b2a9
AlphaNiner1415 <manchesternd@gmail.com>
2021-01-31 21:39:43 +0700

    Update Person.js

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import Aux from "../../../hoc/Aux";
 import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
